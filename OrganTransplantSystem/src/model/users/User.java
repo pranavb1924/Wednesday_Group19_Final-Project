5e890c8193539b84f67d7464f6fa2378e15e512a
Pranav B <bhoopal.p@northeastern.edu>
2024-11-25 15:50:13 -0500

    Final Project Commit - 1 Added OrganTrsansplant System Project

---
+++ OrganTransplantSystem/src/model/users/User.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.users;
+
+/**
+ *
+ * @author pranavb
+ */
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    
+    public User(){
+        
+    }
+    
+    public User(String username, String role){
+        this.username = username;
+        this.role = role;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    } 
+    
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    private String role;
+}
